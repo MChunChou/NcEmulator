81621197be24beba00a9d41ff229b27a122ea8ca
MChunChou <mchunchou89@gmail.com>
2020-08-03 01:49:15 +0800

    Updates

---
+++ precache-manifest.86d68d0904693a465697757d56448d88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62e8b1be844ea3177715206fbc366da1",
+    "url": "/NcEmulator/index.html"
+  },
+  {
+    "revision": "e2278931af88e3e7f549",
+    "url": "/NcEmulator/static/css/main.28ca149a.chunk.css"
+  },
+  {
+    "revision": "252b44dacd4b4e8d0f57",
+    "url": "/NcEmulator/static/js/2.d953a2ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/NcEmulator/static/js/2.d953a2ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2278931af88e3e7f549",
+    "url": "/NcEmulator/static/js/main.fc6081b8.chunk.js"
+  },
+  {
+    "revision": "0feab310c4a4a529f45e",
+    "url": "/NcEmulator/static/js/runtime-main.75a53efe.js"
+  }
+]);
