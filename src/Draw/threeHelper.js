8eba9847c2e5e93590a2aa58b283958732e71c0f
MChunChou <mchunchou89@gmail.com>
2021-02-08 21:30:49 +0800

    Add threeHelper rotate object3d, and changeDom function

--- src/Draw/threeHelper.js
+++ src/Draw/threeHelper.js
@@ -1,26 +1,40 @@
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import {
+    OrbitControls
+} from 'three/examples/jsm/controls/OrbitControls'
 
 export class ThreeHelper {
 
-    constructor(width, height) {
+    constructor(width, height) {             
         this.scene = new THREE.Scene();
-        this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+        this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 3000);
         this.renderer = new THREE.WebGLRenderer();
         this.renderer.setSize(width, height);
-        this.camera.position.set(0, 500, 500);
+        this.camera.position.set(0, 1000, 0);
         this.camera.lookAt(this.scene.position);
         this.scene.background = new THREE.Color(0x000000);
         this.mouseLeftDown = false
         this.mousePosition = []
+        
+        //create object3D 
+        this.object = new THREE.Object3D();
+        this.object.rotateX(-Math.PI / 2);
+        this.scene.add(this.object);
 
-        let controls = new OrbitControls( this.camera, this.renderer.domElement );
-        controls.maxPolarAngle = Math.PI * 0.5;  
+        // add OrbitControls
+        this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+        this.controls.enablePan = false;
+
+        // add axesHelper
+        this.axesHelper = new THREE.AxesHelper(300);
+        this.axesHelper.position.set(0, 0, 5);
+        this.object.add(this.axesHelper);        
     }
 
-    line(points = [], color = 0xdddddd) {
-
-        let material = new THREE.LineBasicMaterial({ color });
+    line(points = [], color = 0xdddd0d) {
+        let material = new THREE.LineBasicMaterial({
+            color
+        });
         let linePoints = [];
 
         for (let i of points) {
@@ -31,29 +45,42 @@ export class ThreeHelper {
         let line = new THREE.Line(geometry, material);
 
         line.name = "drawline";
-        this.scene.add(line.clone())
-
-        console.log(this.scene.children)
+        this.object.add(line.clone())
     }
 
     clear() {
-        this.scene.remove(this.scene.children[1])
+        this.object.remove(this.object.getObjectByName('drawline'))
     }
 
-    animate() {
-        let animate = this.animate;
-        requestAnimationFrame(animate.bind(this));
+    remove(){
+        cancelAnimationFrame(this.animateID);
+    }
+
+    animate() {        
+        let animate = this.animate;                
+        this.animateID = requestAnimationFrame(animate.bind(this));
         this.renderer.render(this.scene, this.camera);
     }
 
-    getRendererDom() {
+    getRendererDom() {        
         return this.renderer.domElement;
     }
 
     coordinate() {
-        let grid = new THREE.GridHelper(500, 50);        
+        let grid = new THREE.GridHelper(1000, 50);
         grid.name = 'coords'
         this.scene.add(grid)
+    }
+
+    setCameraOrigin() {
+        this.camera.position.set(0, 0, 1000);
+    }
+
+    changeDom(width, height){                             
+        this.canvas = this.getRendererDom();
+        this.renderer.setSize(width, height);
+        this.camera.aspect = width / height;
+        this.camera.updateProjectionMatrix();        
     }
 
 }
