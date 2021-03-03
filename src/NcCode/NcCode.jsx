b741f702b79e2477352a2e915529a2d6064ae7c0
MChunChou <mchunchou89@gmail.com>
2021-03-03 16:55:34 +0800

    Add init Code

--- src/NcCode/NcCode.jsx
+++ src/NcCode/NcCode.jsx
@@ -1,7 +1,6 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './index.css'
-const initCode = `
-G90 G00 X -20. Y -20.
+const initCode = `G90 G00 X -20. Y -20.
 G01 X0 Y0
 Y35.
 X20.
@@ -14,11 +13,22 @@ X45. Y10.
 X0 Y0
 X -20. Y -20.
 M02`;
+const initTestCode = `G00 X0.Y0.Z0.
+G02X-100.Y100.Z15.R115.
+G02X0Y100.Z0R55.
+G02X100.Y100.Z15.R55.
+G02X0Y0Z0R115.
+M02`
 
 function NcCode(props) {
-    let {setCode} = props
-    let [line, setLine] = useState(1);
+    let { setCode } = props
+    let [line, setLine] = useState(initTestCode.split(/[\n\r]/g).length);
+    let [value, setValue] = useState(initTestCode);
     let lineRef = useRef(null);
+
+    useEffect(() => {
+        setCode(value)
+    })
 
     function renderLines() {
         let lines = []
@@ -30,27 +40,27 @@ function NcCode(props) {
         return lines
     }
 
-    function handleScroll(evt){            
+    function handleScroll(evt) {
         lineRef.current.scrollTop = evt.target.scrollTop;
     }
-    
+
     function handleOnChange(evt) {
-        let value = evt.target.value
+        let v = evt.target.value
         let lines = value.split(/[\n\r]/g)
-        let linesLen = lines.length;        
-            
+        let linesLen = lines.length;
+
         setLine(linesLen)
-        setCode && setCode(value);
+        setValue(v)
     }
 
-
     return <section className='nccode center'>
-        <div className="line-nums code-style" ref={lineRef}>            
-            {renderLines()}            
+        <div className="line-nums code-style" ref={lineRef}>
+            {renderLines()}
         </div>
         <div className="cnc-code center">
             <textarea
                 className='code-style resize-none'
+                value={value}
                 onChange={handleOnChange}
                 onScroll={handleScroll} />
         </div>
